50eee585031396eb345530c063a8c3176cc45cc0
Hugo-roa <hugo.roav@pca.edu.co>
2026-01-14 19:18:00 -0500

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pcamusic',
+  webDir: 'www'
+};
+
+export default config;
